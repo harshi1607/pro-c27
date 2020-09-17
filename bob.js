c9bdee1d42a3233a183ef07e56080e5c9c139a5e
harshi1607 <69295834+harshi1607@users.noreply.github.com>
2020-09-17 16:10:13 +0530

    Add files via upload

--- bob.js
+++ bob.js
@@ -1,13 +1,13 @@
 class Bob{
     constructor(x,y,radius){
         var bob_options={
-            isStatic : true,
-           // 'restitution': 0.9,//
-            'friction' :0.5,
-            'density' :1.2
+            
+            'restitution': 0.9,
+            'friction' :0,
+            'density' :0.8
        }
        this.body = Bodies.circle(x, y, radius/2, bob_options);
-       this.radius = this.radius;
+       this.radius = radius;
        World.add(world, this.body);
     }
     display(){
@@ -16,7 +16,7 @@ class Bob{
       translate(pos.x,pos.y)
       ellipseMode(CENTER);
       fill("yellow");
-      ellipse(0,0,this.radius)
+      ellipse(0,0,this.radius,this.radius)
       pop();
     }
   
--- sketch.js
+++ sketch.js
@@ -11,26 +11,26 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
 	engine = Engine.create();
-	world = engine.world;
+	world = engine.world; 
 
 	//Create the Bodies Here.
-roof= new Roof(400,200,300,10)
-displayWidth= roof.width/2
-displayHeight= roof.height+500
+roof= new Roof(width/2,height/4,width/7,20)
+startPositionX= width/2
+startPositionY= height/4+500
 bobDiameter=40
-bob1= new Bob(displayWidth-bobDiameter*2,displayHeight,bobDiameter)
-bob2= new Bob(displayWidth-bobDiameter*1,displayHeight,bobDiameter)
-bob3= new Bob(displayWidth,displayHeight,bobDiameter)
-bob4= new Bob(displayWidth+bobDiameter*1,displayHeight,bobDiameter)
-bob5= new Bob(displayWidth+bobDiameter*2,displayHeight,bobDiameter)
+bob1= new Bob(startPositionX-bobDiameter*2,startPositionY,bobDiameter)
+bob2= new Bob(startPositionX-bobDiameter*1,startPositionY,bobDiameter)
+bob3= new Bob(startPositionX,startPositionY,bobDiameter)
+bob4= new Bob(startPositionX+bobDiameter*1,startPositionY,bobDiameter)
+bob5= new Bob(startPositionX+bobDiameter*2,startPositionY,bobDiameter)
 rope1= new Rope(bob1.body,roof.body,-bobDiameter*2,0)
 rope2= new Rope(bob2.body,roof.body,-bobDiameter*1,0)
 rope3= new Rope(bob3.body,roof.body,0,0)
 rope4= new Rope(bob4.body,roof.body,bobDiameter,0)
 rope5= new Rope(bob5.body,roof.body,bobDiameter*2,0)
-	Engine.run(engine);
+	
   
 }
 
@@ -38,6 +38,7 @@ rope5= new Rope(bob5.body,roof.body,bobDiameter*2,0)
 function draw() {
   rectMode(CENTER);
   background("white");
+  Engine.update(engine);
   roof.display();
   bob1.display();
   bob2.display();
@@ -54,6 +55,8 @@ function draw() {
 }
 
 function keyPressed()
- { if (keyCode === UP_ARROW) { Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45}); } }
+ { if (keyCode === UP_ARROW) {
+    Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45}); 
+  } }
 
 
